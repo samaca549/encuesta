c04b8c83a19e0cf349df3fae71da147def13d5d0
Miguel Angel Samaca Rojas <msamacar@unal.edu.co>
2025-09-02 15:49:50 -0500

    Add files via upload

---
+++ encuesta.py
@@ -0,0 +1,11 @@
+# Encuesta simple de ideas de proyecto
+
+for x in range(6):
+    print(f"\nEncuestado {x + 1}")
+    nombre = input("Digite su nombre: ")
+    carrera = input("Digite su carrera: ")
+    idea = input("Digite su idea de proyecto: ")
+
+    print(f"Su nombre es: {nombre}")
+    print(f"Su carrera es: {carrera}")
+    print(f"Su idea es: {idea}")
