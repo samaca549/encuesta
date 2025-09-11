ec027def944161fa7630990766f9f4c608b869d8
samaca549 <msamacar@unal.edu.co>
2025-09-11 15:24:25 -0500

    SE REALIZAN CAMBIOS, SE MEJORA LA ENCUESTA

--- encuesta.py
+++ encuesta.py
@@ -1,11 +1,46 @@
-# Encuesta simple de ideas de proyecto
+class Estudiantes:
+    def __init__(self):
+        self.respuestas = []
 
-for x in range(6):
-    print(f"\nEncuestado {x + 1}")
-    nombre = input("Digite su nombre: ")
-    carrera = input("Digite su carrera: ")
-    idea = input("Digite su idea de proyecto: ")
+    def pedir_datos(self):
+        print("////////////////////////////")
+        print("USUARIO")
+        nombre = input("Digite su nombre: ")
+        proyecto = input("Digite su proyecto: ")
+        trabajo_en_grupo = input("¿Sabe trabajar en grupo?: ")
+        edad = int(input("Digite su edad: "))
+        temas_preferidos = input("¿Qué temas prefiere para el proyecto?: ")
 
-    print(f"Su nombre es: {nombre}")
-    print(f"Su carrera es: {carrera}")
-    print(f"Su idea es: {idea}")
+        return {
+            "nombre": nombre,
+            "proyecto": proyecto,
+            "trabajo_en_grupo": trabajo_en_grupo,
+            "edad": edad,
+            "temas_preferidos": temas_preferidos
+        }
+
+    def agregar_respuesta(self, respuesta):
+        self.respuestas.append(respuesta)
+
+    def mostrar_respuestas(self):
+        print("\n--- Respuestas de los encuestados ---")
+        for i, respuesta in enumerate(self.respuestas, 1):
+            print(f"\nEncuesado #{i}")
+            print(f"Nombre: {respuesta['nombre']}")
+            print(f"Proyecto: {respuesta['proyecto']}")
+            print(f"¿Sabe trabajar en grupo?: {respuesta['trabajo_en_grupo']}")
+            print(f"Edad: {respuesta['edad']}")
+            print(f"Temas preferidos: {respuesta['temas_preferidos']}")
+
+def main ():
+    encuesta = Estudiantes()
+
+    for i in range(11):
+        datos = encuesta.pedir_datos()
+        encuesta.agregar_respuesta(datos)
+
+    encuesta.mostrar_respuestas()
+
+
+if __name__=="__main__":
+    main()
