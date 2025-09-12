5bfc4ac9e6707877baa0514e45e151f7fb8973b6
Miguel Angel Samaca Rojas <msamacar@unal.edu.co>
2025-09-11 21:15:40 -0500

    Update encuesta.py

--- encuesta.py
+++ encuesta.py
@@ -7,40 +7,43 @@ class Estudiantes:
         print("USUARIO")
         nombre = input("Digite su nombre: ")
         proyecto = input("Digite su proyecto: ")
-        trabajo_en_grupo = input("¿Sabe trabajar en grupo?: ")
+        trabajo = input("¿Sabe trabajar en grupo? (Sí/No): ")
         edad = int(input("Digite su edad: "))
-        temas_preferidos = input("¿Qué temas prefiere para el proyecto?: ")
+        temas = input("¿Qué temas prefiere para el proyecto?: ")
 
         return {
             "nombre": nombre,
             "proyecto": proyecto,
-            "trabajo_en_grupo": trabajo_en_grupo,
+            "trabajo": trabajo_en_grupo,
             "edad": edad,
-            "temas_preferidos": temas_preferidos
+            "temas": temas_preferidos
         }
 
     def agregar_respuesta(self, respuesta):
         self.respuestas.append(respuesta)
 
     def mostrar_respuestas(self):
+
         print("\n--- Respuestas de los encuestados ---")
-        for i, respuesta in enumerate(self.respuestas, 1):
-            print(f"\nEncuesado #{i}")
+        contador = 1
+        for respuesta in self.respuestas:
+            print(f"\nEncuestado #{contador}")
+            contador += 1
             print(f"Nombre: {respuesta['nombre']}")
             print(f"Proyecto: {respuesta['proyecto']}")
-            print(f"¿Sabe trabajar en grupo?: {respuesta['trabajo_en_grupo']}")
+            print(f"¿Sabe trabajar en grupo?: {respuesta['trabajo']}")
             print(f"Edad: {respuesta['edad']}")
-            print(f"Temas preferidos: {respuesta['temas_preferidos']}")
+            print(f"Temas preferidos: {respuesta['temas']}")
 
-def main ():
+def main():
     encuesta = Estudiantes()
 
     for i in range(10):
+        print(f"\n--- Ingresando datos del estudiante {i+1} de 10 ---")
         datos = encuesta.pedir_datos()
         encuesta.agregar_respuesta(datos)
 
     encuesta.mostrar_respuestas()
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
